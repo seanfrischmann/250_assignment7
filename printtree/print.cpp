b56f2538f320f87a31b0720971596086c1494e5d
Sean Frischmann <shcsirf@gmail.com>
2013-10-28 10:35:48 -0400

    inital commit

---
+++ printtree/print.cpp
@@ -0,0 +1,53 @@
+// =============================================================================
+// print.cpp
+// ~~~~~~~~~
+// YOUR NAME HERE
+// - implement the three tree printing routines
+// *****************************************************************************
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <stdexcept>
+#include <deque>
+#include "BTree.h"
+#include "term_control.h"
+using namespace std;
+
+// feel free to change the following notice
+string version = 
+    "UB Tree Program. Version 0.1\n"
+    " Author: YOUR NAME\n"
+    " Report bugs to yourid@buffalo.edu\n";
+
+/**
+ * -----------------------------------------------------------------------------
+ * print a given tree vertically
+ * -----------------------------------------------------------------------------
+ */
+void vertical_print(BTNode<string>* root) {
+    // your code goes here
+    cout << "Vertical print: TBD\n";
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * print a given tree horizontally
+ * -----------------------------------------------------------------------------
+ */
+void horizontal_print(BTNode<string>* root) {
+    // your code goes here
+    cout << "Horizontal print: TBD\n";
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * print a given tree symmetrically
+ * -----------------------------------------------------------------------------
+ */
+void symmetric_print(BTNode<string>* root) {
+    // your code goes here
+    cout << "Symmetric print: TBD\n";
+}
