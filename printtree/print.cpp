20ec3a86af26921543433ea62638b391f4c8f9f6
Shcsirf <Shcsirf@gmail.com>
2013-10-29 22:16:12 -0400

    first horizonal print

--- printtree/print.cpp
+++ printtree/print.cpp
@@ -59,13 +59,71 @@ void vprint(BTNode<string>* root, string prefix){
  */
 void vertical_print(BTNode<string>* root) {
     // your code goes here
+	if(root == NULL){
+		cerr << "Please make sure you enter a new tree" << endl;
+		return;
+	}
 	cout << root->payload << endl;
 
-	vprint(root->right, "|");
+	if(root->right != NULL){
+		vprint(root->right, "|");
+	}else if(root->left == NULL){
+		return;
+	}else{
+		cout << "|__x" << endl;
+	}
 	cout << "|" << endl;
-	vprint(root->left, " ");
+	if(root->left != NULL){
+		vprint(root->left, " ");
+	}else{
+		cout << "|__x" << endl;
+	}
 }
 
+/**
+ * -----------------------------------------------------------------------------
+ * find length of left side
+ * -----------------------------------------------------------------------------
+ */
+int length_of_leftside(BTNode<string>* root, int level){
+	int ret=0;
+	int max = root->payload.length();
+	int next;
+	if(root->left != NULL){
+		next = length_of_leftside(root->left, level+1);
+		if(max < next){
+			max = next;
+		}
+	}if(level > 0){
+		return max;
+	}else{
+		ret +=max;
+	}
+	return ret;
+}
+
+/**
+ * -----------------------------------------------------------------------------
+ * find length of right side
+ * -----------------------------------------------------------------------------
+ */
+int length_of_rightside(BTNode<string>* root, int level){
+	int ret=0;
+	if(level
+	int max = root->payload.length();
+	int next;
+	if(root->right != NULL){
+		next = length_of_rightside(root->right, level+1);
+		if(max < next){
+			max = next;
+		}
+	}if(level>1){
+		return max;
+	}else{
+		ret += max;
+	}
+	return ret;
+}
 /**
  * -----------------------------------------------------------------------------
  * print a given tree horizontally
@@ -73,6 +131,8 @@ void vertical_print(BTNode<string>* root) {
  */
 void horizontal_print(BTNode<string>* root) {
     // your code goes here
+	cout << length_of_leftside(root->left, 0) << endl;
+	cout << length_of_rightside(root->left, 0) << endl;
     cout << "Horizontal print: TBD\n";
 }
 
